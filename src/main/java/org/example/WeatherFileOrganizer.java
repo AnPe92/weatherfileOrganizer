c415d0dd5582a6df3c5489d20fb96a42f99c4123
AnPe92 <93088524+AnPe92@users.noreply.github.com>
2023-05-12 00:48:25 +0200

    abit more user friendly

--- src/main/java/org/example/WeatherFileOrganizer.java
+++ src/main/java/org/example/WeatherFileOrganizer.java
@@ -1,33 +1,46 @@
 package org.example;
 
+import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
-public class FileOrganizer
-{
-    //Api key should not be kept like this, only doing it so the app will work with less setup!
-    public static void main( String[] args ) throws IOException {
+public class WeatherFileOrganizer {
 
+    //Instanciating classes that will be used
 
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
+        FileOrganizer fileOrganizer = new FileOrganizer();
         WeatherDataFetcher weatherDataFetcher = new WeatherDataFetcher();
+
+
         boolean running = true;
-        while(running) {
-
-            System.out.println("Hello Cool User!\nPlease enter a location:");
-            String location = scanner.nextLine();
-            if(location.equals("end")){
-                running = false;
+        while (running) {
+            System.out.println("Hello Cool User!\nTime to sort some files!");
+            System.out.println("Choose what you want to do: \n1: sort files\n2: reset sorted files");
+            String userInput = scanner.nextLine();
+            switch (userInput) {
+                case "1":
+                    System.out.println("Enter a PATH:");
+                    String pathInput = scanner.nextLine();
+                    System.out.println("Enter a LOCATION:");
+                    String weatherInput = scanner.nextLine();
+                    String fetchedWeatherData = weatherDataFetcher.fetchWeatherData(weatherInput);
+                    System.out.println(fetchedWeatherData + " <----------------");
+                    //runnning methods
+                    fileOrganizer.moveFiles(pathInput, fetchedWeatherData);
+                    File directory = new File(pathInput);
+                    Desktop.getDesktop().open(directory);
+                    break;
+                case "2":
+                    fileOrganizer.resetFiles();
+                    break;
+                case "exit":
+                    running = false;
+                    break;
             }
-
-
-            System.out.println(" --------------------- ");
-            System.out.println(weatherDataFetcher.fetchWeatherData(location));
-            System.out.println(" --------------------- ");
         }
-        }
+    }
 }
+
